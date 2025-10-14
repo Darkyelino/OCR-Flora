3d9841f401f916992830a6e937aa4a19438dd430
Pablo Cauã da Silva Vasques <abel.vasques2004@gmail.com>
2025-10-14 13:22:54 -0500

    feat: testando novas coisas

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import easyocr
 import os
 
-# Desativa avisos de log que podem poluir a saída
 os.environ['EASYOCR_LOGGER'] = 'ERROR'
 
-# Crie um leitor de OCR para o idioma português
-# O EasyOCR fará o download do modelo do idioma na primeira vez que for executado
 reader = easyocr.Reader(['pt']) 
 
-# Caminho para a sua imagem de teste
-caminho_imagem = 'image.png' 
+caminho_imagem = 'EXEMPLO-REAL.jpg' 
 
-# Extrai o texto da imagem
 resultado = reader.readtext(caminho_imagem, paragraph=True)
 
-# Imprime o texto extraído
 print("--- Texto Bruto Extraído ---")
 for texto in resultado:
-    print(texto[1]) # O texto fica na segunda posição da tupla
+    print(texto[1])
